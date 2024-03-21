cb09875a3ec7692154db946e8ab4a5c56001ec93
AndrewWilliams02 <willi365@sheridancollege.ca>
2024-03-21 15:25:32 -0400

    [TASK-4a] Finished Task4a - Create world-space canvas' for villager's names - Create dropdown to select character instead of  OnMouseDown

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterControl : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI currentSelection;
     public static CharacterControl Instance;
     public static Villager SelectedVillager { get; private set; }
+
+    public List<Villager> availableVillagers;
     public static void SetSelectedVillager(Villager villager)
     {
         if (SelectedVillager != null)
@@ -18,6 +21,15 @@ public class CharacterControl : MonoBehaviour
         SelectedVillager.Selected(true);
         Instance.currentSelection.text = villager.ToString();
     }
+
+    public void CharacterChanged(int index)
+    {
+        if (index >= 0 && index < availableVillagers.Count)
+        {
+            SetSelectedVillager(availableVillagers[index]);
+        }
+    }
+
     private void Start()
     {
         Instance = this;
